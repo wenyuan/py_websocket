ecfbd1a2fa601eaa0232a435010595e7f5060a00
wenyuan <xuwenyuan2016@163.com>
2018-10-10 17:41:40 +0800

    :bug: fix-bug

--- server.py
+++ server.py
@@ -13,9 +13,9 @@ clients = {}
 
 
 # 通知客户端
-def notify(message):
+def notify_msg(msg):
     for connection in clients.values():
-        connection.send('%c%c%s' % (0x81, len(message), message))
+        connection.send('%c%c%s' % (0x81, len(msg), msg))
 
 
 # 客户端处理线程
@@ -31,24 +31,17 @@ class WebSocketThread(Thread):
         # @https://www.cnblogs.com/JetpropelledSnake/p/9033064.html
         data = self.connection.recv(1024)
         headers = self.parse_headers(data)
-        if 'Sec-WebSocket-Key' not in headers :
+        if 'Sec-WebSocket-Key' not in headers:
             print('This socket is not websocket, client close')
             self.connection.close()
             return
-        magic_key = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-        token = self.generate_token(headers['Sec-WebSocket-Key'], magic_key)
-
+        token = self.generate_token(headers['Sec-WebSocket-Key'])
         response = 'HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n' \
                    'Connection: Upgrade\r\n' \
                    'Upgrade: WebSocket\r\n' \
                    'Sec-WebSocket-Accept: {token}\r\n' \
                    'WebSocket-Protocol: chat\r\n\r\n'.format(token=token)
-        # self.connection.send(response)
-        self.connection.send('\
-        HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n\
-        Upgrade: WebSocket\r\n\
-        Connection: Upgrade\r\n\
-        Sec-WebSocket-Accept: %s\r\n\r\n' % token)
+        self.connection.send(response)
 
         # 进行通信
         while True:
@@ -58,54 +51,41 @@ class WebSocketThread(Thread):
                 print('unexpected error: ', e)
                 clients.pop(self.username)
                 break
-            data = self.parse_data(data)
+            raw_data = self.parse_data(data)
             if len(data) == 0:
                 continue
-            # self.conn.send(data)
-            message = self.username + ": " + data
-            notify(message)
+            msg = self.username + " says: " + raw_data
+            notify_msg(msg)
 
     def parse_data(self, data):
-        msg_len = data[1] & 127    # 数据载荷的长度
+        msg_len = ord(data[1]) & 127  # 数据载荷的长度
         if msg_len == 126:
-            mask = data[4:8]       # Mask掩码
-            content = data[8:]     # 消息内容
+            mask = data[4:8]  # Mask掩码
+            content = data[8:]  # 消息内容
         elif msg_len == 127:
             mask = data[10:14]
             content = data[14:]
         else:
             mask = data[2:6]
             content = data[6:]
-        raw_str = ''    # 解码后的内容
+        raw_data = ''  # 解码后的内容
         for i, d in enumerate(content):
-            raw_str += chr(d ^ mask[i % 4])
-        return raw_str
+            raw_data += chr(ord(d) ^ ord(mask[i % 4]))
+        return raw_data
 
-
-
-        # v = ord(msg[1]) & 0x7f
-        # if v == 0x7e:
-        #     p = 4
-        # elif v == 0x7f:
-        #     p = 10
-        # else:
-        #     p = 2
-        # mask = msg[p:p + 4]
-        # data = msg[p + 4:]
-        # return ''.join([chr(ord(v) ^ ord(mask[k % 4])) for k, v in enumerate(data)])
-
-    def parse_headers(self, data):
-        headers_dict = {}
-        headers, _ = data.split('\r\n\r\n', 1)
-        for line in headers.split('\r\n')[1:]:
+    def parse_headers(self, msg):
+        headers = {}
+        header, data = msg.split('\r\n\r\n', 1)
+        for line in header.split('\r\n')[1:]:
             key, value = line.split(': ', 1)
-            headers_dict[key] = value
-        headers_dict['data'] = _
-        return headers_dict
+            headers[key] = value
+        headers['data'] = data
+        return headers
 
-    def generate_token(self, sec_key, magic_key):
-        key = sec_key + magic_key
-        return base64.b64encode(hashlib.sha1(key).digest())
+    def generate_token(self, msg):
+        key = msg + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+        ser_key = hashlib.sha1(key).digest()
+        return base64.b64encode(ser_key)
 
 
 # 服务端
@@ -129,9 +109,9 @@ class WebSocketServer(object):
         print('websocket server running...')
         # 等待访问
         while True:
-            connection, address  = sock.accept()  # 此时会进入waiting状态
+            connection, address = sock.accept()  # 此时会进入waiting状态
             try:
-                username = "ID" + str(address [1])
+                username = "ID" + str(address[1])
                 thread = WebSocketThread(connection, username)
                 thread.start()
                 clients[username] = connection
@@ -140,5 +120,5 @@ class WebSocketServer(object):
 
 
 if __name__ == '__main__':
-    websocket_server = WebSocketServer('127.0.0.1', 9000)
-    websocket_server.start()
+    server = WebSocketServer('127.0.0.1', 9000)
+    server.start()
