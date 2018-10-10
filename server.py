012a3f78eba2e4d6b0bb08633bde15b21b197351
wenyuan <xuwenyuan2016@163.com>
2018-10-10 16:07:33 +0800

    :bug: fix bug

--- server.py
+++ server.py
@@ -20,58 +20,79 @@ def notify(message):
 
 # 客户端处理线程
 class WebSocketThread(Thread):
-    def __init__(self, conn, addr):
+    def __init__(self, connection, username):
         super(WebSocketThread, self).__init__()
-        self.conn = conn
-        self.addr = addr
+        self.connection = connection
+        self.username = username
 
     def run(self):
         print('new websocket client joined!')
         # 服务端响应报文
         # @https://www.cnblogs.com/JetpropelledSnake/p/9033064.html
-        data = self.conn.recv(1024)
-        headers_dict = self.parse_headers(data)
-        if 'Sec-WebSocket-Key' not in headers_dict:
+        data = self.connection.recv(1024)
+        headers = self.parse_headers(data)
+        if 'Sec-WebSocket-Key' not in headers :
             print('This socket is not websocket, client close')
-            self.conn.close()
+            self.connection.close()
             return
         magic_key = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-        token = self.generate_token(headers_dict['Sec-WebSocket-Key'], magic_key)
+        token = self.generate_token(headers['Sec-WebSocket-Key'], magic_key)
 
         response = 'HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n' \
                    'Connection: Upgrade\r\n' \
                    'Upgrade: WebSocket\r\n' \
                    'Sec-WebSocket-Accept: {token}\r\n' \
                    'WebSocket-Protocol: chat\r\n\r\n'.format(token=token)
-        self.conn.send(response)
+        # self.connection.send(response)
+        self.connection.send('\
+        HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n\
+        Upgrade: WebSocket\r\n\
+        Connection: Upgrade\r\n\
+        Sec-WebSocket-Accept: %s\r\n\r\n' % token)
 
         # 进行通信
         while True:
             try:
-                data = self.conn.recv(1024)
+                data = self.connection.recv(1024)
             except socket.error as e:
                 print('unexpected error: ', e)
-                self.conn.close()
+                clients.pop(self.username)
                 break
-            msg = self.parse_data(data)
-            if len(msg) == 0:
+            data = self.parse_data(data)
+            if len(data) == 0:
                 continue
-            self.conn.send(msg)
+            # self.conn.send(data)
+            message = self.username + ": " + data
+            notify(message)
 
-    def parse_data(self, msg):
-        print('=========')
-        print(msg)
-        print('=========')
-        v = ord(msg[1]) & 0x7f
-        if v == 0x7e:
-            p = 4
-        elif v == 0x7f:
-            p = 10
+    def parse_data(self, data):
+        msg_len = data[1] & 127    # 数据载荷的长度
+        if msg_len == 126:
+            mask = data[4:8]       # Mask掩码
+            content = data[8:]     # 消息内容
+        elif msg_len == 127:
+            mask = data[10:14]
+            content = data[14:]
         else:
-            p = 2
-        mask = msg[p:p + 4]
-        data = msg[p + 4:]
-        return ''.join([chr(ord(v) ^ ord(mask[k % 4])) for k, v in enumerate(data)])
+            mask = data[2:6]
+            content = data[6:]
+        raw_str = ''    # 解码后的内容
+        for i, d in enumerate(content):
+            raw_str += chr(d ^ mask[i % 4])
+        return raw_str
+
+
+
+        # v = ord(msg[1]) & 0x7f
+        # if v == 0x7e:
+        #     p = 4
+        # elif v == 0x7f:
+        #     p = 10
+        # else:
+        #     p = 2
+        # mask = msg[p:p + 4]
+        # data = msg[p + 4:]
+        # return ''.join([chr(ord(v) ^ ord(mask[k % 4])) for k, v in enumerate(data)])
 
     def parse_headers(self, data):
         headers_dict = {}
@@ -108,12 +129,12 @@ class WebSocketServer(object):
         print('websocket server running...')
         # 等待访问
         while True:
-            conn, addr = sock.accept()  # 此时会进入waiting状态
+            connection, address  = sock.accept()  # 此时会进入waiting状态
             try:
-                # username = "ID" + str(addr[1])
-                thread = WebSocketThread(conn, addr)
+                username = "ID" + str(address [1])
+                thread = WebSocketThread(connection, username)
                 thread.start()
-                # clients[username] = conn
+                clients[username] = connection
             except socket.timeout:
                 print('websocket connection timeout!')
 
