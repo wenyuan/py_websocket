3f71f779c66e25bfa2ba1be6480b119c31565991
wenyuan <xuwenyuan2016@163.com>
2018-10-10 15:48:35 +0800

    :sparkles: server code

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
-# -*- coding:utf8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import threading
+# ref: @https://www.jianshu.com/p/3f42172f582b
+
 import hashlib
 import socket
 import base64
+from threading import Thread
 
 global clients
 clients = {}
@@ -16,36 +19,49 @@ def notify(message):
 
 
 # 客户端处理线程
-class websocket_thread(threading.Thread):
-    def __init__(self, connection, username):
-        super(websocket_thread, self).__init__()
-        self.connection = connection
-        self.username = username
+class WebSocketThread(Thread):
+    def __init__(self, conn, addr):
+        super(WebSocketThread, self).__init__()
+        self.conn = conn
+        self.addr = addr
 
     def run(self):
-        print 'new websocket client joined!'
-        data = self.connection.recv(1024)
-        headers = self.parse_headers(data)
-        token = self.generate_token(headers['Sec-WebSocket-Key'])
-        self.connection.send('\
-HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n\
-Upgrade: WebSocket\r\n\
-Connection: Upgrade\r\n\
-Sec-WebSocket-Accept: %s\r\n\r\n' % token)
+        print('new websocket client joined!')
+        # 服务端响应报文
+        # @https://www.cnblogs.com/JetpropelledSnake/p/9033064.html
+        data = self.conn.recv(1024)
+        headers_dict = self.parse_headers(data)
+        if 'Sec-WebSocket-Key' not in headers_dict:
+            print('This socket is not websocket, client close')
+            self.conn.close()
+            return
+        magic_key = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+        token = self.generate_token(headers_dict['Sec-WebSocket-Key'], magic_key)
+
+        response = 'HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n' \
+                   'Connection: Upgrade\r\n' \
+                   'Upgrade: WebSocket\r\n' \
+                   'Sec-WebSocket-Accept: {token}\r\n' \
+                   'WebSocket-Protocol: chat\r\n\r\n'.format(token=token)
+        self.conn.send(response)
+
+        # 进行通信
         while True:
             try:
-                data = self.connection.recv(1024)
-            except socket.error, e:
-                print "unexpected error: ", e
-                clients.pop(self.username)
+                data = self.conn.recv(1024)
+            except socket.error as e:
+                print('unexpected error: ', e)
+                self.conn.close()
                 break
-            data = self.parse_data(data)
-            if len(data) == 0:
+            msg = self.parse_data(data)
+            if len(msg) == 0:
                 continue
-            message = self.username + ": " + data
-            notify(message)
+            self.conn.send(msg)
 
     def parse_data(self, msg):
+        print('=========')
+        print(msg)
+        print('=========')
         v = ord(msg[1]) & 0x7f
         if v == 0x7e:
             p = 4
@@ -57,44 +73,51 @@ Sec-WebSocket-Accept: %s\r\n\r\n' % token)
         data = msg[p + 4:]
         return ''.join([chr(ord(v) ^ ord(mask[k % 4])) for k, v in enumerate(data)])
 
-    def parse_headers(self, msg):
-        headers = {}
-        header, data = msg.split('\r\n\r\n', 1)
-        for line in header.split('\r\n')[1:]:
+    def parse_headers(self, data):
+        headers_dict = {}
+        headers, _ = data.split('\r\n\r\n', 1)
+        for line in headers.split('\r\n')[1:]:
             key, value = line.split(': ', 1)
-            headers[key] = value
-        headers['data'] = data
-        return headers
+            headers_dict[key] = value
+        headers_dict['data'] = _
+        return headers_dict
 
-    def generate_token(self, msg):
-        key = msg + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-        ser_key = hashlib.sha1(key).digest()
-        return base64.b64encode(ser_key)
+    def generate_token(self, sec_key, magic_key):
+        key = sec_key + magic_key
+        return base64.b64encode(hashlib.sha1(key).digest())
 
 
 # 服务端
-class websocket_server(threading.Thread):
-    def __init__(self, port):
-        super(websocket_server, self).__init__()
+class WebSocketServer(object):
+    def __init__(self, ip, port):
+        self.ip = ip
         self.port = port
 
-    def run(self):
+    def start(self):
+        # 创建 tcp socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 设置socket.close()后马上释放该端口,否则默认会经过一个TIME_WAIT
+        # @http://www.360doc.com/content/12/0321/14/6312609_196283700.shtml
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('127.0.0.1', self.port))
-        sock.listen(5)
-        print 'websocket server started!'
+        try:
+            sock.bind((self.ip, self.port))
+            sock.listen(5)
+        except Exception as e:
+            print('server error!')
+            print(e)
+        print('websocket server running...')
+        # 等待访问
         while True:
-            connection, address = sock.accept()
+            conn, addr = sock.accept()  # 此时会进入waiting状态
             try:
-                username = "ID" + str(address[1])
-                thread = websocket_thread(connection, username)
+                # username = "ID" + str(addr[1])
+                thread = WebSocketThread(conn, addr)
                 thread.start()
-                clients[username] = connection
+                # clients[username] = conn
             except socket.timeout:
-                print 'websocket connection timeout!'
+                print('websocket connection timeout!')
 
 
 if __name__ == '__main__':
-    server = websocket_server(9000)
-    server.start()
+    websocket_server = WebSocketServer('127.0.0.1', 9000)
+    websocket_server.start()
