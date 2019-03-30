11d1b2ce95055271e0978baf18f5fdc1dbb18c85
wenyuan <xuwenyuan2016@163.com>
2019-03-30 19:39:16 +0800

    :pencil: rewrite demo

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # PY WebSocket
-> python实现简单的websocket通讯。 </br>
-> 一个多人聊天室的雏形
+> 基于Web页面实时推送技术的demo</br>
 
 ## 使用方法
-运行命令：python server.py </br>
-打开：chatroom.html </br>
+### 1. 基于HTTP
+运行命令：python http_server.py </br>
+轮询（polling）：打开：http_polling_client.html </br>
+长轮询（long-polling）：打开：http_long_polling_client.html </br>
+iframe流（iframe-streaming）：打开：http_iframe_streaming_client.html </br>
+
+### 2. 基于WebSocket
+运行命令：python websocket_server.py </br>
+打开：websocket_client.html </br>
 多打开几个页面，连接、发送消息 </br>
 
 ## 备注
--- http_server.py
+++ http_server.py
@@ -1,43 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-import cgi
+import json
 import time
+import urlparse
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 
 class MyHttpHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path != '/':
-            self.send_error(404, 'File not found.')
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            # send html message
+            self.wfile.write('Hello World')
             return
+        if self.path == '/get_current_time/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            # send html message(for iframe-streaming)
+            count = 0
+            while True:
+                current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                html_string = """<script type="text/javascript">
+                parent.document.getElementById('current-time').innerHTML = '{current_time}';
+                </script>
+                """.format(current_time=current_time)
+                self.wfile.write(html_string)
+                count += 1
+                print('iframe-streaming response count: {count}'.format(count=count))
+                time.sleep(1)
+        elif self.path.startswith('/get_current_time/'):
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            # self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        # send html message,
-        self.wfile.write('Hello World')
-        return
-
-    def do_POST(self):
-        ctype, pdict = cgi.parse_header(self.headers['content-type'])
-        if ctype == 'application/json':
-            post_data = dict(
-                current_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            )
+            # send json message
+            parsed_path = urlparse.urlparse(self.path)
+            params = urlparse.parse_qs(parsed_path.query)
+            callback = params.get('callback')[0]
+            result = {
+                'code': 200,
+                'data': dict(
+                    current_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                )
+            }
+            jsonp_result = callback + "(" + json.dumps(result) + ")"
+            self.wfile.write(jsonp_result)
+            return
         else:
-            self.send_error(415, "Only json data is supported.")
+            self.send_error(404, 'Page not found.')
             return
-
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-        self.wfile.write(post_data)
 
 
 if __name__ == '__main__':
-    server = HTTPServer(('127.0.0.1', 9000), MyHttpHandler)
+    server = HTTPServer(('localhost', 9000), MyHttpHandler)
     print("Starting server, use <Ctrl-C> to stop")
     server.serve_forever()
